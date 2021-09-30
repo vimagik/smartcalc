eb639b293c9b02e48740b43c0fa5c11f220e4589
Rzhavin Georgii <rzhavin@my.com>
2021-09-30 10:50:44 +0300

    First create

---
+++ src/components/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container class="my-lg-auto">
+    <v-row>
+      <v-col align="center">
+        <v-card max-width="500">
+
+          <div class="text-h5">Приветствую тебя, мой дорогой друг</div>
+          <spacer></spacer>
+
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+
+</template>
+
+<script>
+export default {
+  name: "Main"
+}
+</script>
+
+<style scoped>
+
+</style>
