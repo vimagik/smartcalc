2144b0cedb6e273743843dc88d4846d17b781d23
UbuntuX <vimagik@gmail.com>
2021-10-03 01:08:48 +0300

    Created the exercise component

---
+++ src/components/ExerciseComp.vue
@@ -0,0 +1,38 @@
+<template>
+  <tr>
+    <td>1)</td>
+    <td>5</td>
+    <td>*</td>
+    <td>5</td>
+    <td>=</td>
+    <td>
+      <v-text-field
+          class="mt-6"
+          label="Ответ"
+          outlined
+          dense
+      ></v-text-field>
+    </td>
+    <td>
+      <v-btn
+          color="primary"
+          elevation="2"
+          fab
+          small
+          dark
+      >
+        <v-icon>mdi-check-all</v-icon>
+      </v-btn>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "ExerciseComp"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/QuizComp.vue
+++ src/components/QuizComp.vue
@@ -7,8 +7,65 @@
           <div class="pa-2 pt-6 pb-6">
 
             <div class="text-h5 text-center">Приветствую тебя, {{ login }}!</div>
+            <v-simple-table class="mt-10">
+              <tbody>
+              <exercise-comp/>
+              <exercise-comp/>
+              <exercise-comp/>
+              <tr>
+                <td>1)</td>
+                <td>5</td>
+                <td>*</td>
+                <td>5</td>
+                <td>=</td>
+                <td>
+                  <v-text-field
+                      class="mt-6"
+                      label="Ответ"
+                      outlined
+                      dense
+                      disabled
+                  ></v-text-field>
+                </td>
 
 
+                <td>
+                  <v-alert
+                      class="mt-3"
+                      type="error"
+                      dense
+                      width="55"
+                  ></v-alert>
+                </td>
+              </tr>
+              <tr>
+                <td>2)</td>
+                <td>2</td>
+                <td>*</td>
+                <td>2</td>
+                <td>=</td>
+                <td>
+                  <v-text-field
+                      class="mt-6"
+                      label="Ответ"
+                      outlined
+                      dense
+                  ></v-text-field>
+                </td>
+                <td>
+                  <v-btn
+                      color="primary"
+                      elevation="2"
+                      fab
+                      small
+                      dark
+                  >
+                    <v-icon>mdi-check-all</v-icon>
+                  </v-btn>
+                </td>
+              </tr>
+              </tbody>
+            </v-simple-table>
           </div>
         </v-card>
       </v-col>
@@ -17,25 +74,20 @@
 </template>
 
 <script>
+import ExerciseComp from "@/components/ExerciseComp";
+
 export default {
   name: "QuizComp",
+  components: {
+    ExerciseComp
+  },
   props: {
     login: String,
     operationType: String,
     numberOfExamples: String
   },
-  methods: {
-    execute() {
-      const abc = this.$router;
-      console.log(abc);
-      console.log(this.login);
-      console.log(this.operationType);
-      console.log(this.numberOfExamples);
-    }
-  },
-  mounted() {
-    this.execute();
-  }
+  methods: {},
+
 }
 </script>
 
