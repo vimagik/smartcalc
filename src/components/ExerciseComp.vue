ee1da09072a06b80326decc6fdbc62f54dea539a
Rzhavin Georgii <rzhavin@my.com>
2021-10-03 22:30:26 +0300

    Alfa version

--- src/components/ExerciseComp.vue
+++ src/components/ExerciseComp.vue
@@ -1,35 +1,99 @@
 <template>
   <tr>
-    <td>1)</td>
-    <td>5</td>
-    <td>*</td>
-    <td>5</td>
-    <td>=</td>
+    <td>
+      <v-icon large color="blue darken-1">mdi-numeric-{{ number }}-box</v-icon>
+    </td>
+    <td class="text-h6">{{ firstNumber }}</td>
+    <td class="text-h6">{{ operation }}</td>
+    <td class="text-h6">{{ secondNumber }}</td>
+    <td class="text-h6">=</td>
     <td>
       <v-text-field
+          v-model="inputResult"
           class="mt-6"
           label="Ответ"
           outlined
           dense
+          :disabled="replyReceived"
+          @keydown.enter="processResponse"
       ></v-text-field>
     </td>
     <td>
-      <v-btn
-          color="primary"
-          elevation="2"
-          fab
-          small
-          dark
+      <v-btn v-if="!replyReceived"
+             color="primary"
+             elevation="2"
+             fab
+             small
+             @click="processResponse"
+             :disabled="!inputResult"
       >
         <v-icon>mdi-check-all</v-icon>
       </v-btn>
+      <template v-else>
+        <v-alert v-if="correctResponse"
+            class="mt-3"
+            type="success"
+            dense
+            width="55"
+        ></v-alert>
+        <v-alert v-else
+            class="mt-3"
+            type="error"
+            dense
+            width="55"
+        ></v-alert>
+      </template>
     </td>
   </tr>
 </template>
 
 <script>
 export default {
-  name: "ExerciseComp"
+  name: "ExerciseComp",
+  props: {
+    number: String,
+    operation: String
+  },
+  emits: ['sendresult'],
+  data() {
+    return {
+      firstNumber: null,
+      secondNumber: null,
+      inputResult: null,
+      replyReceived: false,
+      correctResponse: null
+    }
+  },
+  methods: {
+    processResponse() {
+      if (this.operation === '*') {
+        this.correctResponse = this.firstNumber * this.secondNumber === Number(this.inputResult) ? true : false;
+      } else {
+        this.correctResponse = this.firstNumber + this.secondNumber === Number(this.inputResult) ? true : false;
+      }
+      this.replyReceived = true;
+      this.$emit('sendresult', this.correctResponse);
+    },
+
+    firstInit() {
+      if (this.operation === '*') {
+        this.firstNumber = this.getRandomInt(0, 10);
+        this.secondNumber = 2;
+      } else {
+        this.firstNumber = this.getRandomInt(0, 15);
+        this.secondNumber = this.getRandomInt(0, 15);
+      }
+    },
+
+    getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+  },
+  mounted() {
+    this.firstInit();
+  }
 }
 </script>
 
--- src/components/QuizComp.vue
+++ src/components/QuizComp.vue
@@ -9,61 +9,8 @@
             <div class="text-h5 text-center">Приветствую тебя, {{ login }}!</div>
             <v-simple-table class="mt-10">
               <tbody>
-              <exercise-comp/>
-              <exercise-comp/>
-              <exercise-comp/>
-              <tr>
-                <td>1)</td>
-                <td>5</td>
-                <td>*</td>
-                <td>5</td>
-                <td>=</td>
-                <td>
-                  <v-text-field
-                      class="mt-6"
-                      label="Ответ"
-                      outlined
-                      dense
-                      disabled
-                  ></v-text-field>
-                </td>
-
-
-                <td>
-                  <v-alert
-                      class="mt-3"
-                      type="error"
-                      dense
-                      width="55"
-                  ></v-alert>
-                </td>
-              </tr>
-              <tr>
-                <td>2)</td>
-                <td>2</td>
-                <td>*</td>
-                <td>2</td>
-                <td>=</td>
-                <td>
-                  <v-text-field
-                      class="mt-6"
-                      label="Ответ"
-                      outlined
-                      dense
-                  ></v-text-field>
-                </td>
-                <td>
-                  <v-btn
-                      color="primary"
-                      elevation="2"
-                      fab
-                      small
-                      dark
-                  >
-                    <v-icon>mdi-check-all</v-icon>
-                  </v-btn>
-                </td>
-              </tr>
+              <exercise-comp v-for="item of exercises" :key="item" v-on:sendresult="collectResult" :number="item"
+                             :operation="operationType"/>
               </tbody>
             </v-simple-table>
           </div>
@@ -74,6 +21,14 @@
 </template>
 
 <script>
+
+// [ ] Кнопка, начать с cамого начала
+// [ ] Кнопка, начать с начала
+// [V] Не давать нажимать кнопку, пока не ввели значение
+// [ ] Красиво вывести итого
+// [ ] Добавить анимацию
+// [ ] Добавить вывод номера больше 10
+
 import ExerciseComp from "@/components/ExerciseComp";
 
 export default {
@@ -86,7 +41,25 @@ export default {
     operationType: String,
     numberOfExamples: String
   },
-  methods: {},
+  data() {
+    return {
+      result: 0,
+      exercises: [1]
+    }
+  }
+  ,
+  methods: {
+    collectResult(st) {
+      if (st) {
+        this.result += 1;
+      }
+      if (this.exercises.length < Number(this.numberOfExamples)) {
+        this.exercises.push(this.exercises.length + 1);
+      } else {
+        alert(`Правильных ответов ${this.result}`);
+      }
+    }
+  },
 
 }
 </script>
