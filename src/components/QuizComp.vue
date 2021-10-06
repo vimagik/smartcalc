09c9e92fbd85dbcc585e7d7f3f97c8a049286430
Rzhavin Georgii <rzhavin@my.com>
2021-10-06 20:48:37 +0300

    Add some features

--- src/components/QuizComp.vue
+++ src/components/QuizComp.vue
@@ -9,23 +9,63 @@
             <div class="text-h5 text-center">Приветствую тебя, {{ login }}!</div>
             <v-simple-table class="mt-10">
               <tbody>
-              <exercise-comp v-for="item of exercises" :key="item" v-on:sendresult="collectResult" :number="item"
+              <exercise-comp v-for="item of exercises" :key="item.id" v-on:sendresult="collectResult"
+                             :number="item.number"
                              :operation="operationType"/>
               </tbody>
             </v-simple-table>
+
+            <div class="pt-5" v-if="testIsFinished">
+              <v-btn @click="startQuiz">
+                Начать заново
+              </v-btn>
+
+              <v-btn class="ml-5" color="primary" @click="returnToMain">
+                Вернуться на главный экран
+              </v-btn>
+            </div>
+
           </div>
         </v-card>
       </v-col>
+    </v-row>
+
+
+    <v-row justify="center">
+      <v-dialog
+          v-model="dialog"
+          persistent
+          max-width="350"
+      >
+        <v-card>
+          <v-card-title class="text-h5">
+            Поздравляю, {{ login }}!
+          </v-card-title>
+          <v-card-text>
+            Правильных ответов {{ result }} из {{ numberOfExamples }}.
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                color="green darken-1"
+                text
+                @click="dialog = false"
+            >
+              Спасибо
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-row>
   </v-container>
 </template>
 
 <script>
 
-// [ ] Кнопка, начать с cамого начала
-// [ ] Кнопка, начать с начала
+// [V] Кнопка, начать с cамого начала
+// [V] Кнопка, начать с начала
 // [V] Не давать нажимать кнопку, пока не ввели значение
-// [ ] Красиво вывести итого
+// [V] Красиво вывести итого
 // [ ] Добавить анимацию
 // [ ] Добавить вывод номера больше 10
 
@@ -44,19 +84,43 @@ export default {
   data() {
     return {
       result: 0,
-      exercises: [1]
+      exercises: [
+        {
+          id: 0,
+          number: 1
+        }
+      ],
+      testIsFinished: false,
+      dialog: null
     }
   }
   ,
   methods: {
+    returnToMain() {
+      this.$router.go(-1);
+    },
+    startQuiz() {
+      this.exercises.push({
+        id: this.exercises[this.exercises.length - 1].id + 1,
+        number: 1
+      });
+
+      this.exercises = this.exercises.slice(this.exercises.length - 1);
+      this.testIsFinished = false;
+      this.result = 0;
+    },
     collectResult(st) {
       if (st) {
         this.result += 1;
       }
       if (this.exercises.length < Number(this.numberOfExamples)) {
-        this.exercises.push(this.exercises.length + 1);
+        this.exercises.push({
+          id: this.exercises[this.exercises.length - 1].id + 1,
+          number: this.exercises.length + 1
+        });
       } else {
-        alert(`Правильных ответов ${this.result}`);
+        this.testIsFinished = true;
+        this.dialog = true;
       }
     }
   },
